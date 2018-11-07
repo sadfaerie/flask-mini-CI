59ef953015f8ec7e8059234c090ac7e3e67c85bc
sadfaerie <sadfaerie@gmail.com>
2018-11-07 16:21:01 +0000

    added basic routes

---
+++ README.md
@@ -0,0 +1,1 @@
+Flask chat app - following the how-to from codeinstitute.net
---
+++ run.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    """Main page with instructions"""
+    return "To send a message use /USERNAME/MESSAGE"
+
+
+@app.route("/<username>")
+def user(username):
+    return "Hi " + username
+
+@app.route("/<username>/<message>")
+def send_message(username, message):
+    return "{0}: {1}".format(username, message)
+
+if __name__ == "__main__":
+    app.run(port=5003,debug=True)
